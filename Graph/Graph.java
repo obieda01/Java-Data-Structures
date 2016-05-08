b44322cc366ae5b130b968f5ba36cbd61efa88e0
Ahmad Obiedat <obieda01@email.franklin.edu>
2016-05-08 10:25:43 -0400

    Update Graph.java

--- Graph/Graph.java
+++ Graph/Graph.java
@@ -12,7 +12,6 @@ import java.util.PriorityQueue;
 import java.util.Queue;
 import java.util.Set;
 
-
 public class Graph<Type> {
     private HashSet<Type> vertex;
     private HashMap<Type, LinkedList<Edge<Type>>> adj;
